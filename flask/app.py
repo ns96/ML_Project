0932a3c247a4490b6de04a24b2b2ccb728575bb0
Nathan Stevens <nathan.stevens@instras.com>
2024-02-07 14:21:59 -0500

    First pass at defining the flask API endpoints

--- flask/app.py
+++ flask/app.py
@@ -46,28 +46,22 @@ def welcome():
     
     return (
         f"<b>Available Routes:</b><br/><br>"
-        f"<b>/api/v1.0/drg/&lt;drg_id&gt;</b> [ get all DRG records group by state ]<br/>"
-        f"<b>/api/v1.0/categories</b> [ get the main categories ]<br/>"
-        f"<b>/api/v1.0/definitions/&lt;category&gt;</b> [ get all the DRG definitions for a particular category ]<br/>"
-        f"<b>/api/v1.0/stats</b> [ get basic DRG statistics group by state ]<br/>"
-        f"<b>/api/v1.0/top/providers/&lt;limit&gt;</b> [ get top n providers by patient discharges nation wide ]<br/>"        
-        f"<b>/api/v1.0/providers/&lt;state&gt;</b> [ get all providers in a state ]<br/>"
-        f"<b>/api/v1.0/providers/&lt;state&gt;/&lt;drg_id&gt; </b>[ get all providers in a state for a particular DRG ]<br/><br/>"
+        f"<b>/api/v1.0/utilities</b> [ get average install cost by utility ]<br/>"
+        f"<b>/api/v1.0/installers</b> [ get average install cost by installers ]<br/>"
+        f"<b>/api/v1.0/cities</b> [ get average install cost by cities ]<br/>"
+        f"<b>/api/v1.0/zipcodes</b> [ get average install cost by zipcodes ]<br/>"
+        f"<b>/api/v1.0/city/&lt;city&gt</b> [ get average install cost for a city ]<br/>"
+        f"<b>/api/v1.0/zipcode/&lt;zipcode&gt</b> [ get average install cost for zip code ]<br/>"
+        f"<b>/api/v1.0/estimate/&lt;zipcode&gt;/&lt;kwh&gt;/&lt;technology&gt; </b>[ get cost estimate by zip code and KWh ]<br/><br/>"
         f"<b>/view </b>[ <a href='/view'> view the basic web UI </a>]<br/>"
     )
 
-@app.route("/api/v1.0/drg/<drg_id>")
-def drg_all(drg_id):
+@app.route("/api/v1.0/utilities")
+def cost_by_utilities():
     """
-    Return the drg data for all the states
+    Return the avergage install cost by utilities
     """
-    query = text('SELECT "Provider State", COUNT(*), SUM("Total Discharges"), '\
-                 'AVG("Average Total Payments"), AVG("Average Medicare Payments") '\
-                 'FROM "DRG_RECORDS" '\
-                 'INNER JOIN "PROVIDERS" '\
-                 'ON "PROVIDERS"."Provider Id" = "DRG_RECORDS"."Provider Id" '\
-                 'WHERE "DRG Id" = ' + drg_id + ' '\
-                 'GROUP BY "Provider State"')
+    query = text('')
     print(query)
     
     with engine.connect() as conn:
@@ -92,10 +86,10 @@ def drg_all(drg_id):
     
     return jsonify(records)
 
-@app.route("/api/v1.0/categories")
-def drg_categories():
+@app.route("/api/v1.0/installers")
+def cost_by_installers():
     """ 
-    Return the drg categories
+    Return the avergage install cost by installers
     """
     query = text('SELECT "Category", COUNT(*) '\
                  'FROM "DRG" '\
@@ -111,12 +105,12 @@ def drg_categories():
     
     return jsonify(categories)
 
-@app.route("/api/v1.0/definitions/<category>")
-def drg_definitions(category):
+@app.route("/api/v1.0/cities")
+def cost_by_cities():
     """ 
-    Return the drg definitions for a particular category
+    Return the avergage install cost by cities
     """
-    query = text("SELECT * FROM \"DRG\" WHERE \"Category\" = " + "'" + category + "'")
+    query = text("")
     print(query)
     with engine.connect() as conn:
       results = conn.execute(query).fetchall()
@@ -130,61 +124,51 @@ def drg_definitions(category):
     
     return jsonify(definitions)
 
-@app.route("/api/v1.0/stats")
-def drg_stats():
+@app.route("/api/v1.0/zipcodes")
+def cost_by_zipcodes():
+    """ 
+    Return the avergage install cost by zipcodes
     """
-    Return the stats nation wide group by the drg so you can what the most
-    popular surgery/procedure is etc
-    """
-    
-    query = text('SELECT MIN("DRG"."DRG Id"), "DRG Definition", '\
-                 'COUNT(*), SUM("Total Discharges"), '\
-                 'AVG("Average Total Payments"), '\
-                 'AVG("Average Medicare Payments") '\
-                 'FROM "DRG_RECORDS" '\
-                 'INNER JOIN "DRG" '\
-	             'ON "DRG"."DRG Id" = "DRG_RECORDS"."DRG Id" '\
-                 'GROUP BY "DRG Definition" ')
+    query = text("")
     print(query)
-    
     with engine.connect() as conn:
       results = conn.execute(query).fetchall()
     
-    records = dict()
-    
+    definitions = []
     for row in results:
-      record = dict()
-      record["drg_id"] = int(row[0])
-      record["drg_definition"] = row[1]
-      record["count"] = row[2]
-      record["discharges"] = row[3]
-      record["avg_payments"] = int(row[4])
-      record["avg_medicare"] = int(row[5])
-      record["avg_difference"] = int(row[4] - row[5])
-      
-      # calcuate the percent medicare payments
-      pct = int((row[5]/row[4])*100)
-      record["pct_medicare"] = pct
-      
-      records[int(row[0])] = record # add to dictionary
+      definition = dict()
+      definition["value"] = row[0]
+      definition["text"] = row[1]
+      definitions.append(definition)
     
-    return jsonify(records)
+    return jsonify(definitions)
 
-@app.route("/api/v1.0/top/providers/<limit>")
-def top_providers(limit):
+@app.route("/api/v1.0/city/<city>")
+def cost_for_city(city):
+    """ 
+    Return the avergage install cost for a particular city
     """
-    Return the the top n providers nation wide grouping by state and provider
+    query = text("")
+    print(query)
+    with engine.connect() as conn:
+      results = conn.execute(query).fetchall()
+    
+    definitions = []
+    for row in results:
+      definition = dict()
+      definition["value"] = row[0]
+      definition["text"] = row[1]
+      definitions.append(definition)
+    
+    return jsonify(definitions)
+
+@app.route("/api/v1.0/zipcode/<zipcode>")
+def cost_for_zipcode(zipcode):
     """
-    query = text('SELECT "Provider State", "Provider Name", COUNT(*), SUM("Total Discharges"), '\
-                 'AVG("Average Total Payments"), AVG("Average Medicare Payments"), '\
-                 'MIN("latitude"), MIN("longitude") '\
-                 'FROM "DRG_RECORDS" '\
-                 'INNER JOIN "PROVIDERS" '\
-	             'ON "PROVIDERS"."Provider Id" = "DRG_RECORDS"."Provider Id" '\
-                 'INNER JOIN "DRG" '\
-	             'ON "DRG"."DRG Id" = "DRG_RECORDS"."DRG Id" '\
-                 'GROUP BY "Provider State", "Provider Name" '\
-                 'ORDER BY SUM("Total Discharges") DESC LIMIT ' + limit)
+    Return the avergage install cost for a particular zipcode
+    """
+    query = text('')
+    
     print(query)
     
     with engine.connect() as conn:
@@ -211,63 +195,13 @@ def top_providers(limit):
       providers.append(provider)
     
     return jsonify(providers)
-
-@app.route("/api/v1.0/providers/<state>")
-def providers_for_state(state):
-    """
-    Return the all providers for a particular state
-    """
-    query = text('SELECT "Provider Name", COUNT(*), SUM("Total Discharges"), '\
-                 'AVG("Average Total Payments"), AVG("Average Medicare Payments"), '\
-                 'MIN("latitude"), MIN("longitude") '\
-                 'FROM "DRG_RECORDS" '\
-                 'INNER JOIN "PROVIDERS" '\
-	             'ON "PROVIDERS"."Provider Id" = "DRG_RECORDS"."Provider Id" '\
-                 'INNER JOIN "DRG" '\
-	             'ON "DRG"."DRG Id" = "DRG_RECORDS"."DRG Id" '\
-                 "WHERE \"Provider State\" = '" + state + "' "\
-                 'GROUP BY "Provider Name"')
-    print(query)
-    
-    with engine.connect() as conn:
-      results = conn.execute(query).fetchall()
-    
-    providers = []
-    for row in results:
-      provider = dict()
-      
-      provider["state"] = state
-      provider["name"] = row[0]
-      provider["drg_count"] = row[1]
-      provider["discharges"] = row[2]
-      provider["avg_payments"] = int(row[3])
-      provider["avg_medicare"] = int(row[4])
-      provider["avg_difference"] = int(row[3] - row[4])
-      
-      # calcuate the percent medicare payments
-      pct = int((row[4]/row[3])*100)
-      provider["pct_medicare"] = pct
-      provider["latitude"] = row[5]
-      provider["longitude"] = row[6]
-      
-      providers.append(provider)
-    
-    return jsonify(providers)
   
-@app.route("/api/v1.0/providers/<state>/<drg_id>")
-def providers(state, drg_id):
+@app.route("/api/v1.0/estimate/<zipcode>/<kwh>/<technology>")
+def get_estimate(zipcode, kwh, technology):
     """
-    Return the providers in a state for a particular drg
+    Return an estimate given the zipcode. desired, kwh and technology
     """
-    query = text('SELECT "Provider State", "Provider Name", "DRG Definition", '\
-	             '"Total Discharges", "Average Total Payments", '\
-	             '"Average Medicare Payments", "latitude", "longitude" '\
-                 'FROM "DRG_RECORDS" '\
-                 'INNER JOIN "PROVIDERS" '\
-	             'ON "PROVIDERS"."Provider Id" = "DRG_RECORDS"."Provider Id" '\
-                 'INNER JOIN "DRG" '\
-	             'ON "DRG"."DRG Id" = "DRG_RECORDS"."DRG Id" '\
-                 "WHERE \"Provider State\" = '" + state + "' AND \"DRG_RECORDS\".\"DRG Id\" = " + drg_id)
+    query = text('')
     print(query)
     
     with engine.connect() as conn:
@@ -277,7 +211,6 @@ def providers(state, drg_id):
     for row in results:
       provider = dict()
       
-      provider["drg_id"] = drg_id
       provider["state"] = row[0]
       provider["name"] = row[1]
       provider["drg"] = row[2]
